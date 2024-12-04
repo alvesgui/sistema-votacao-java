5614ab5c2fd03dc11116ff51b8e6aa868aae7b4e
Guilherme Alves <guylherme.alves14@gmail.com>
2024-12-04 19:16:20 -0300

    Java Class Voto

--- src/main/java/com/sistemavotacao/entity/Voto.java
+++ src/main/java/com/sistemavotacao/entity/Voto.java
@@ -1,7 +1,15 @@
 package com.sistemavotacao.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Voto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
